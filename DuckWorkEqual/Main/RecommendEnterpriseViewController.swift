f0e19f0a618e95a47e98eee88d9d17ca5a60f9bc
권준상 <wnstkdsun97@naver.com>
2021-11-24 23:21:21 +0900

    기업 추천 VC 구현

--- DuckWorkEqual/Main/RecommendEnterpriseViewController.swift
+++ DuckWorkEqual/Main/RecommendEnterpriseViewController.swift
@@ -9,9 +9,84 @@ import UIKit
 
 class RecommendEnterpriseViewController: UIViewController {
 
+    var mainCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        
+        $0.backgroundColor = .colorWithRGBHex(hex: 0xF4F3F3)
+        $0.showsVerticalScrollIndicator = false
+        $0.collectionViewLayout = layout
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .colorWithRGBHex(hex: 0xF4F3F3)
+        initMainCollectionView()
+        setNavBar()
+        setLayouts()
+    }
+    
+    func initMainCollectionView() {
+        mainCollectionView.delegate = self
+        mainCollectionView.dataSource = self
+        mainCollectionView.register(EnterpriseCell.self, forCellWithReuseIdentifier: "EnterpriseCell")
+    }
+    
+    func setNavBar() {
+        navigationController?.isNavigationBarHidden = false
+        navigationItem.title = "#탐구형"
+        navigationItem.setHidesBackButton(true, animated: true)
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.shadowImage = UIImage()
+    }
+    
+    private func setLayouts() {
+        setViewHierarchy()
+        setConstraints()
+    }
+    
+    private func setViewHierarchy() {
+        view.addSubviews(mainCollectionView)
+    }
+    
+    private func setConstraints() {
+        let guide = view.safeAreaLayoutGuide
+        
+        mainCollectionView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(guide)
+        }
+    }
+    
+    @objc func scrapButtonDidTap(sender: UIButton) {
+        if !sender.isSelected {
+            sender.isSelected = true
+        } else {
+            sender.isSelected = false
+        }
     }
 
+}
+
+extension RecommendEnterpriseViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width - 20 , height: 165)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EnterpriseCell", for: indexPath) as? EnterpriseCell
+        else {
+            return UICollectionViewCell()
+        }
+        
+        cell.scrapButton.addTarget(self, action: #selector(scrapButtonDidTap), for: .touchUpInside)
+        return cell
+    }
+    
+    
 }
